c0dce49a63ab297e43e6014acbb9a5a7ea5bb45b
FemzEin <femzein@gmail.com>
2022-09-15 09:16:35 -0700

    Update Debugging Task

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * main - calls other functions
- *
- * Return:Always 0
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
 
 int main(void)
