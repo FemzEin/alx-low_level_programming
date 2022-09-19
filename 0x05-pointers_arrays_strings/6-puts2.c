5f9295d00f839fa29eb3bd5e9b9b37e97f361965
FemzEin <femzein@gmail.com>
2022-09-19 12:34:01 -0700

    Update

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,12 +9,13 @@
 
 void puts2(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (*(str + i))
+	while (str[i] != '\0')
 	{
-		_putchar(*(str + i));
-		i = i + 2;
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
 	}
-	_putchar('\n');
+		_putchar('\n');
 }
