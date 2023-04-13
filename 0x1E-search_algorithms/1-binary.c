f8186d51fd116a9c0c381a06144be873d8ec61c8
Albert James <femzein@gmail.com>
2023-04-13 10:29:01 -0700

    Search algorithms update

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,53 +1,67 @@
 #include "search_algos.h"
 
 /**
-* print_array - Print the array
-* @array: The array to print
-* @first: The first index
-* @last: The last index
-*/
+ * print_array - Print the array
+ * @array: The array to print
+ * @first: The first index
+ * @last: The last index
+ */
 void print_array(int *array, size_t first, size_t last)
 {
+	size_t i = first;
+	int start = 1;
+
 	printf("Searching in array: ");
-	for (size_t i = first; i <= last; i++)
-	printf("%d%s", array[i], i < last ? ", " : "\n");
+	for (; i <= last; i++)
+	{
+		if (start)
+		{
+			printf("%i", array[i]);
+			start = 0;
+			continue;
+		}
+		printf(", %i", array[i]);
+	}
+	printf("\n");
 }
 
 /**
-* bs_imp - Implementation of binary search
-* @array: The array to search through
-* @first: First index
-* @last: Last index
-* @value: The value to search for
-*
-* Return: The index of the value or -1
-*/
+ * bs_imp - Implementation of binary search
+ * @array: The array to search through
+ * @first: First index
+ * @last: Last index
+ * @value: The value to search for
+ *
+ * Return: The index of the value or -1
+ */
 int bs_imp(int *array, size_t first, size_t last, int value)
 {
 	size_t mid = (first + last) / 2;
 
-print_array(array, first, last);
+	print_array(array, first, last);
 	if (array[mid] == value)
-	return (mid);
+		return (mid);
 	if (first == last)
-	return (-1);
+		return (-1);
 	if (value < array[mid])
-	return (bs_imp(array, first, mid - 1, value));
+		return (bs_imp(array, first, mid, value));
 	else
-	return (bs_imp(array, mid + 1, last, value));
+		return (bs_imp(array, mid + 1, last, value));
 }
 
 /**
-* binary_search - Binary Search algorithm
-* @array: The array to search through
-* @size: The size of the array
-* @value: The value to search for
-*
-* Return: The index of the value or -1
-*/
+ * binary_search - Binary Search algorithm
+ * @array: The array to search through
+ * @size: The size of the array
+ * @value: The value to search for
+ *
+ * Return: The index of the value or -1
+ */
 int binary_search(int *array, size_t size, int value)
 {
-	if (array == NULL || size == 0)
-	return (-1);
-	return (bs_imp(array, 0, size - 1, value));
+	size_t last_idx = size - 1;
+
+	if (array == NULL)
+		return (-1);
+	return (bs_imp(array, (size_t)0, last_idx, value));
 }
